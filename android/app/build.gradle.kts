ee0c8510923486ed8a57c6c826672bf82ff2eb60
thssnm <thssn@posteo.de>
2026-03-18 09:52:40 +0100

    Enable core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -61,9 +61,10 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    isCoreLibraryDesugaringEnabled = true  
+}
 }
 
 dependencies {
